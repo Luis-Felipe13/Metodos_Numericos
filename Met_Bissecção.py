460c4ab7561ea692f02bfe2036a71e05b2687786
Luis-Felipe13 <luisfelipeismaelsilvatte@gmail.com>
2025-10-18 17:56:00 -0300

    Update

--- Met_Bissecção.py
+++ Met_Bissecção.py
@@ -1,23 +1,56 @@
 import math
-import numpy as np
 
-func = input("Digite o f(x) na seguinte formatação: x**(n-1)n +- x**(n-1) +- C**(n-2)")
-
-pi = int(input("Digite o ponto inicial:"))
-pf = int(input("DIgite o ponto final:"))
+tol = 0.0001
+func = input("Digite o f(x) na seguinte formatação (ex: x**2 + 2*x - 3):")
+pi = float(input("Digite o ponto inicial:"))
+pf = float(input("DIgite o ponto final:"))
 
 f = eval(f"lambda x: {func}", {"math": math})
+
 cont = pi
 find_sqrt = False
+a = pi
+b = pf
+f_a_inicial = f(a)
+
 while cont < pf:
-    fpi = f(cont)
-    fpi2 = f(cont+1)
-    cont+=1
-    if fpi * fpi2 < 0:
+
+    cont_prox = min(cont + 1.0, pf)
+    f_cont_prox = f(cont_prox)
+
+    if f_a_inicial * f_cont_prox <= 0:
         find_sqrt = True
-        print('Raiz encontrada em:')
-        print(cont-1, fpi, fpi2)
+        a = cont
+        b = cont_prox
         break
 
+    a = cont_prox
+    f_a_inicial = f_cont_prox
+    cont += 1.0
+
 if not find_sqrt:
-    print('Essa função está incorreta.')
+    print('Não há raiz, ou a função está errada')
+else:
+    iteracao = 0
+    fa = f(a)
+
+    while (b - a) > tol:
+        iteracao += 1
+
+        m = (a + b) / 2
+        f_m = f(m)
+
+        if abs(f_m) < 1e-10:
+            a = m
+            b = m
+            break
+
+        if fa * f_m < 0:
+            b = m
+        else:
+            a = m
+            fa = f_m
+
+r_a = (a + b) / 2
+print(f"Raiz aproximada: {r_a:.6f}")
+print(f"Iterações: {iteracao}")
