a86d24ba439bc15766d0192e16ed488831d825b4
Luis Felipe <luisfelipeismaelsilvatte@gmail.com>
2025-11-10 23:18:06 -0300

    Comentarios

--- 1.0/metodos/fechados.py
+++ 1.0/metodos/fechados.py
@@ -2,20 +2,27 @@
 
 def isolamento_raiz(f, pi, pf, passo=1.0):
     """Realiza a busca incremental no intervalo [pi, pf]."""
+    #Passo 1:
     a = pi
+    #Passo 1.1:
     f_a_inicial = f(a)
 
+    #Passo 2:
     while a < pf:
+        #Passo 2.1:
         cont_prox = min(a + passo, pf)
 
+        #Questão de segurança, para não entrar em loop infinito
         if a == cont_prox:
             break
-
+            
+        #Passo 1.1 (Novamente)
         f_cont_prox = f(cont_prox)
 
+        #Passo 3:
         if f_a_inicial * f_cont_prox <= 0:
             return a, cont_prox
-
+        #Passo 4:
         a = cont_prox
         f_a_inicial = f_cont_prox
 
@@ -31,16 +38,21 @@ def metodo_bisseccao(f, a_inicial, b_inicial, tol, max_iter=50):
     b = b_inicial
     fa = f(a)
 
+    #Verificação de segurança
     if fa * f(b) > 0:
         raise ValueError("f(a) e f(b) devem ter sinais opostos.")
-
+    
     iteracoes = []
     iteracao_cont = 0
 
+    #Passo 1:
     while (b - a) > tol and iteracao_cont < max_iter:
         iteracao_cont += 1
-
+        
+        #Passo 2:
         m = (a + b) / 2
+        
+        #Passo 3:
         f_m = f(m)
 
         iteracoes.append({
@@ -51,16 +63,19 @@ def metodo_bisseccao(f, a_inicial, b_inicial, tol, max_iter=50):
             'f(m)': f"{f_m:.6f}",
             '|b-a|': f"{abs(b - a):.6f}"
         })
-
+        #Verificação de segurança (Se atendida, quebra loop)
         if abs(f_m) < 1e-10:
             break
 
+        #Passo 3.1:
         if fa * f_m < 0:
             b = m
         else:
             a = m
             fa = f_m
+        #Volta ao loop
 
+    #Final da iteração
     r_a = (a + b) / 2
     return r_a, iteracoes
 
